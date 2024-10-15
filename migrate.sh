8909e95c4867a199d2ecf808bfd0fbc5cad0ff55
YootTanA <oatyootna@gmail.com>
2024-10-16 01:03:20 +0700

    Remove unused script

--- migrate.sh
+++
@@ -1,9 +0,0 @@
-dirs=$(ls  -l | grep -v resnet50 | grep -v kustomize | grep '^d' | awk '{print $9}')
-
-for d in $dirs; do 
-  echo "-----"
-  echo $d
-  find $d/ -type f -exec sed -i "s/DEFAULT/IMAGENET1K_V1/g" {} \;
-
-  echo "-----"
-done;
