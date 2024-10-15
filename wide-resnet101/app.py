3c530b2810ca575b2001308354f63a55e1db93d1
YootTanA <oatyootna@gmail.com>
2024-10-16 01:02:22 +0700

    Fix wrong weight

--- wide-resnet101/app.py
+++ wide-resnet101/app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 from flask_injector import FlaskInjector
 from PIL import Image
 from torchvision.io import read_image
-from torchvision.models import wide-resnet101, Wide_ResNet101_2_Weights
+from torchvision.models import wide_resnet101_2, Wide_ResNet101_2_Weights
 import torchvision.transforms as transforms
 
 
@@ -57,7 +57,7 @@ class AppModule(Module):
 
        # Load model weights when start up.
        weights = Wide_ResNet101_2_Weights.IMAGENET1K_V1
-       model = wide-resnet101(weights=weights)
+       model = wide_resnet101_2(weights=weights)
 
        binder.bind(PredictionService, to=PredictionService(model, weights), scope=None)
 
